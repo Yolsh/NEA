c5536a2c15736f3170acfc76091e6d4544c969ae
23JA1168 <23JA1168@L2757>
2024-06-27 10:16:26 +0100

    randoms

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -12,20 +12,24 @@ namespace Prototype
         public static Garage MyGarage;
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a minimum Hallway Width");
-            MyGarage = new Garage(20, 10, int.Parse(Console.ReadLine()), 2, 90, 0, 0);
-            TestPlace();
-            Console.ReadKey();
+            for (; ; )
+            {
+                Console.WriteLine("Enter a minimum Hallway Width");
+                MyGarage = new Garage(20, 10, int.Parse(Console.ReadLine()), 2, 90, 0, 0);
+                TestPlace();
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
 
         static void TestPlace()
         {
             MyGarage
-            .AddBoxes("Box1", 2.2, 3, 3, 2, 2)
-            .AddBoxes("Box2", 1.3, 3, 3, 2, 2)
-            .AddBoxes("Box3", 4.5, 1, 1, 2, 2)
-            .AddBoxes("Box4", 0.3, 2, 2, 2, 2)
-            .AddBoxes("Box5", 6.67, 5, 5, 2, 2);
+            .AddBoxes("Box1", 2.2, rand.Next(1, 5), rand.Next(1, 5), 2, 2)
+            .AddBoxes("Box2", 1.3, rand.Next(1, 5), rand.Next(1, 5), 2, 2)
+            .AddBoxes("Box3", 4.5, rand.Next(1, 5), rand.Next(1, 5), 2, 2)
+            .AddBoxes("Box4", 0.3, rand.Next(1, 5), rand.Next(1, 5), 2, 2)
+            .AddBoxes("Box5", 6.67, rand.Next(1, 5), rand.Next(1, 5), 2, 2);
             Console.Clear();
             MyGarage.Draw();
             Console.ReadKey();
@@ -34,3 +38,8 @@ namespace Prototype
         }
     }
 }
+
+/*
+ * error handling for overflowing boxes
+ * maybe better explain the problem.
+ */
